c87c75304184ee074f5361a36c008a07e069f033
milee yu <mileeyu@gmail.com>
2021-04-17 21:16:01 -0700

    Add scrollX component

--- js/cocobits.accordion.js
+++ js/cocobits.accordion.js
@@ -1,30 +1,32 @@
 /*! ACCORDION */
 
 function accordion() {
-  const accButtons = document.querySelectorAll('.accordion'),
-        py = 48
+  if (document.querySelector('.accordion')) {
+    const accButtons = document.querySelectorAll('.accordion'),
+      py = 48
   
-  // Listen for click
-  accButtons.forEach(button => {
-    button.addEventListener('click', function () {
-      button.classList.toggle('accordion-active')
+    // Listen for click
+    accButtons.forEach(button => {
+      button.addEventListener('click', function () {
+        button.classList.toggle('accordion-active')
 
-      if (button.classList.contains('accordion-active')) {
-        button.querySelector('.plus').style.setProperty('--plus-after', '90deg')
-      } else {
-        button.querySelector('.plus').style.setProperty('--plus-after', '0deg')
-      }
-      let content = button.nextElementSibling
+        if (button.classList.contains('accordion-active')) {
+          button.querySelector('.plus').style.setProperty('--plus-after', '90deg')
+        } else {
+          button.querySelector('.plus').style.setProperty('--plus-after', '0deg')
+        }
+        let content = button.nextElementSibling
 
-      if (content.style.maxHeight) {
-        content.style.maxHeight = null
-        content.style.padding = '0px 16px'
-      } else {
-        content.style.maxHeight = `${content.scrollHeight + py}px`
-        content.style.padding = '24px 16px'
-      }
+        if (content.style.maxHeight) {
+          content.style.maxHeight = null
+          content.style.padding = '0px 16px'
+        } else {
+          content.style.maxHeight = `${content.scrollHeight + py}px`
+          content.style.padding = '24px 16px'
+        }
+      })
     })
-  })
+  } else { return }
 }
 
 module.exports = { accordion }
